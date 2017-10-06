10d5a34af0f60f2adf82e07ab1ee08e666b7e657
corychiang <corychiang@iii.org.tw>
2017-10-06 10:01:55 +0800

    Bug fix

--- json.c
+++ json.c
@@ -3,6 +3,37 @@
 * 
 * by. Cory Chiang
 * 
+
+BSD 3-Clause License
+
+Copyright (c) 2017, Cory Chiang
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 ******/
 
 #include <stdlib.h>
@@ -13,9 +44,16 @@
 
 #include "json.h"
 
+///TODO: When parsing, detect empty array or object, that will cause phrase error.
+
 /* forward reference declaration */
 void _SJSNbuildVAL(struct SJSN_SHELL* sh);
 char* _SJSNexportVAL(struct SJSN_VAL* val, char* buf, int size, int* err);
+
+void _SJSNskipBLANK(struct SJSN_SHELL* sh);
+unsigned int _SJSNhashIDX(char* name);
+
+struct SJSN_OBJ* _SJSNObjMultiQuery_last_ptr=NULL;
 
 /* parsing */
 inline void _SJSNskipBLANK(struct SJSN_SHELL* sh)
@@ -130,7 +168,7 @@ void _SJSNmatchNUMBER(struct SJSN_SHELL* sh)
 	}
 	else {
 		sh->p_stack[sh->p_level]->type=SJSN_VAL_INTEGER;
-		sh->p_stack[sh->p_level]->res.integer=atoi(num);
+		sh->p_stack[sh->p_level]->res.integer=atoll(num);
 	}
 }
 
@@ -756,6 +794,8 @@ struct SJSN_VAL* SJSNQuery(struct SJSN_VAL* val, char* path, int mode)
 	struct SJSN_ARR *aptr;
 	struct SJSN_OBJ *optr, *optr_p;
 	
+	if( !val || !path ) return NULL;
+	
 	i=0;
 	
 	while(1) {
@@ -850,6 +890,31 @@ struct SJSN_VAL* SJSNQuery(struct SJSN_VAL* val, char* path, int mode)
 	}
 	
 	return val;
+}
+
+struct SJSN_VAL* SJSNObjMultiQuery(struct SJSN_VAL* val, char* id)
+{
+	struct SJSN_OBJ *optr;
+	struct SJSN_VAL *rval;
+	unsigned int i;
+	
+	if(!val) {
+		if(!_SJSNObjMultiQuery_last_ptr) return NULL;
+		else optr=_SJSNObjMultiQuery_last_ptr;
+	}
+	else {
+		if(val->type!=SJSN_VAL_OBJECT) return NULL;
+		else optr=val->res.object;
+	}
+	
+	i=_SJSNhashIDX(id);
+	
+	while(optr) {
+		if(optr->hidx==i) return optr->value;
+		else optr=optr->next;
+	}
+	
+	return NULL;
 }
 
 unsigned int SJSNhidx(char* str)
