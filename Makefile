8ab0c08c60cc40bc17c2a280a79dd87d29ac0fcd
root <root@CoryInspiron5515>
2025-04-16 23:56:17 +0800

    Clean old codes

--- Makefile
+++ Makefile
@@ -3,10 +3,7 @@ CC = gcc
 all:
 	$(CC) -c json.c
 	$(CC) json_demo.c json.o -lm -o json_demo
-	#$(CC) json_demo_adv.c json.o -lm -o json_demo_adv
 
 clean:
 	rm *.o
 	-rm json_demo
-	#-rm json_demo_adv
-
--- json.h
+++ json.h
@@ -1,9 +1,9 @@
 /******
-* JSON Parser & Exporter
+* JSON Parser & Utilities
 * 
 * by. Cory Chiang
 * 
-*   V. 2.2.1 (2023/07/10)
+*   V. 3.0.0 (2025/04/16)
 *
 
 BSD 3-Clause License
@@ -43,29 +43,12 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <stdint.h>
 #include <stdbool.h>
-#include <errno.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define SJSN_ENABLE_QUERY	1
-#define SJSN_NUM_POWER		1
-#define SJSN_NUM_INT		0
-
-#define SJSN_MAX_LEVEL		32
-
 /* data types */
-#define SJSN_VAL_NULL		0
-#define SJSN_VAL_FALSE		1
-#define SJSN_VAL_TRUE		2
-#define SJSN_VAL_OBJECT		3
-#define SJSN_VAL_ARRAY		4
-#define SJSN_VAL_REALNUM	5
-#define SJSN_VAL_INTEGER	6
-#define SJSN_VAL_STRING		7
-
-// for 3.0
 #define JSON_TYPE_NULL     0
 #define JSON_TYPE_BOOLEAN  1
 #define JSON_TYPE_STRING   2
@@ -73,24 +56,11 @@ extern "C" {
 #define JSON_TYPE_NUMERIC  4
 #define JSON_TYPE_ARRAY    5
 #define JSON_TYPE_OBJECT   6
-// --
 
 /* error codes */
-#define SJSN_OK				0
-#define SJSN_PHRASE_ERROR	1
-#define SJSN_SYNTAX_ERROR	2
-#define SJSN_LEVEL_EXCEED	3
-#define SJSN_UNEXP_SUFFIX	4
-#define SJSN_UNEXP_NUL		5
-#define SJSN_OUT_OF_BUF		6
-#define SJSN_EXPORT_TYPE	7
-
-// for 3.0
 #define JSON_ERROR_NONE    0
 #define JSON_ERRPR_PHRASE  1  
-// --
 
-// for 3.0
 typedef struct _json_t {
     uint8_t type;
     union {  // this should be 64 bits / 8 bytes in modern 64-bit systems
@@ -104,62 +74,6 @@ typedef struct _json_t {
 	char *label;
 } json_t;
 
-/*typedef struct _json_labeled_t {
-    json_t value;
-    char *label;
-} json_labeled_t;*/
-// --
-
-// forward declaration
-/*struct SJSN_VA;
-struct SJSN_VB;
-*/
-/* data definitions */
-
-// data descriptors
-/*
-union SJSNval
-{
-	char*				string;
-	int64_t				integer;
-	double				realnum;
-	struct SJSN_VA*		array;
-	struct SJSN_VB*		object;
-};
-
-struct SJSN_VA
-{
-	uint8_t			type;
-	union SJSNval	data;
-	struct SJSN_VA*	next;
-};
-
-struct SJSN_VB
-{
-	uint8_t			type;
-	union SJSNval	data;
-	struct SJSN_VB*	next;
-	
-	char*			name;
-	uint32_t		hidx;
-};
-
-struct SJSN_SHELL
-{
-	char*			str;
-	
-	int				error_code;
-	int				p_ptr;
-	int				p_level;
-	struct SJSN_VA*	p_stack[SJSN_MAX_LEVEL];
-};
-*/
-/* functions */
-/*
-struct SJSN_VA* SJNSParse(struct SJSN_SHELL* sh);
-struct SJSN_VA* SJNSQuickParse(char* src);
-*/
-// for 3.0
 bool jsonFillNull(json_t *dst);
 bool jsonFillBoolean(json_t *dst, bool value);
 bool jsonFillString(json_t *dst, char *value);
@@ -178,22 +92,7 @@ double jsonGetNumeric(json_t *value);
 char *jsonGetString(json_t *value);
 
 void jsonFree(json_t *value);
-// --
-/*
-int SJSNExport(struct SJSN_VA* val, char* buf, int size);
-void SJSNFree(void* val);
 
-void SJSNObjectIdx(struct SJSN_VB* obj);
-
-#if defined(SJSN_ENABLE_QUERY) && SJSN_ENABLE_QUERY
-
-struct SJSN_VA* SJSNQuery(struct SJSN_VA* val, char* path, int mode);
-struct SJSN_VA* SJSNObjMultiQuery(struct SJSN_VA* val, char* id);
-
-#endif
-
-unsigned int SJSNhidx(char* str);
-*/
 #ifdef __cplusplus
 }
 #endif
